3f2bebe040bbc03f3e63a04935822c998beb7afe
rubencm33 <rubencm545@gmail.com>
2025-11-06 14:54:14 +0100

    agregar configuración centralizada con Pydantic - SECRET_KEY y ALGORITHM para JWT - DATABASE_URL para conexión a DB - Variables cargadas desde archivo .env

---
+++ app/config.py
@@ -0,0 +1,18 @@
+# app/config.py
+import os
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# 👇 Fuerza la carga manual del .env (aunque PyCharm no lo lea automáticamente)
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+settings = Settings()
+
